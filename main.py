9fa8e6d4ce6478f589fd406da3b7bc61f3104779
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2025-05-20 14:56:19 +0800

    Updates

--- main.py
+++ main.py
@@ -19,59 +19,49 @@ color_sensors = (
     ColorSensor(Port.C),
 )
 db = DriveBase(left_motor, right_motor, 62.4, 160)
-db.settings(straight_speed=500, straight_acceleration=3000, turn_rate=350, turn_acceleration=3000)
+db.settings(straight_speed=500, straight_acceleration=3000, turn_rate=350, turn_acceleration=800)  # max turn acceleration 3000
 
 SENSOR_POSITIONS = (-3, -1, 1, 3)
 
 
-class PD:
-    """Proportional-derivative controller for line tracking."""
+class PIController:
+    """Proportional-integral controller for line tracking."""
 
-    def __init__(self, k_p: float, k_d: float):
+    def __init__(self, k_p: float, k_i: float):
         """Init."""
         self.K_P = k_p
-        self.K_D = k_d
-        self.DERIVATIVE_WINDOW = 10
-        self.rolling_errors = [0] * self.DERIVATIVE_WINDOW
-        self.rolling_times = [0] * self.DERIVATIVE_WINDOW
-        self.error_pointer = 0
-        self.rolling_filled = False
+        self.K_I = k_i
+        self.integral = 0
+        self.integral_max = 100
         self.stopwatch = StopWatch()
         self.ticks = 0
+        self.error_positive = True
 
     def update(self, error: float) -> float:
         """Get the PID output for a new error value."""
-        stopwatch_time = self.stopwatch.time()
-
         # Proportional term
         p_term = self.K_P * error
 
-        # Derivative term
-        if stopwatch_time - self.rolling_times[self.error_pointer] <= 0:
-            d_term = 0
-        elif not self.rolling_filled:
-            d_term = 0
-        else:
-            d_term = (
-                self.K_D
-                * (error - self.rolling_errors[self.error_pointer])
-                / (stopwatch_time - self.rolling_times[self.error_pointer])
-            )
+        # Integral term
+        # self.integral += error
+        # self.integral = max(min(self.integral, self.integral_max), -self.integral_max)
+        # if self.error_positive and error < 0:
+        #     self.integral = 0
+        #     self.error_positive = False
+        # elif not self.error_positive and error > 0:
+        #     self.integral = 0
+        #     self.error_positive = True
+        # i_term = self.K_I * self.integral
 
-        # Update errors
-        self.rolling_errors[self.error_pointer] = error
-        self.rolling_times[self.error_pointer] = stopwatch_time
-        self.error_pointer = (self.error_pointer + 1) % self.DERIVATIVE_WINDOW
-        if not self.rolling_filled and self.error_pointer == 0:
-            self.rolling_filled = True
+        i_term = 0
 
         # Output readings
-        if self.ticks >= 100:
-            print(stopwatch_time, error, p_term, d_term)
-            self.ticks = 0
-        self.ticks += 1
+        # if self.ticks >= 100:
+        # print(error, p_term, i_term)
+        # self.ticks = 0
+        # self.ticks += 1
 
-        return p_term + d_term
+        return p_term + i_term
 
 
 def process_reflections(reflections: tuple[int, int, int, int]) -> tuple[float, float, float, float]:
@@ -86,19 +76,23 @@ def process_reflections(reflections: tuple[int, int, int, int]) -> tuple[float, 
     return tuple(out)
 
 
-def linetrack(min_distance: int | float, speed: int | float, *, direction: str = "none", junctions: int = 1) -> None:
+def linetrack(
+    min_distance: int | float,
+    speed: int | float,
+    *,
+    direction: str = "none",
+    junctions: int = 1,
+    intercept_direction: str = "right",
+) -> None:
     """Line track using PID."""
     junctions_crossed = 0
     junction_size = 20
     junction_distance = -junction_size
-    distance_indicator = False
-    junction_indicator = False
-    ran_off = False
-    off_distance = 0
-    hub.display.pixel(0, 0, 0)
-    hub.display.pixel(0, 1, 0)
-    pid_controller = PD(40, 0)  # FIXME: ...
+    pid_controller = PIController(50, 1)  # FIXME: ...
     db.reset()
+
+    intercept = False
+    slow = False
 
     while True:
         line_amounts = process_reflections(tuple(sensor.reflection() for sensor in color_sensors))
@@ -117,18 +111,6 @@ def linetrack(min_distance: int | float, speed: int | float, *, direction: str =
         else:
             raise ValueError("Invalid direction")
 
-        # Show distance reached indicator
-        if distance_reached and not distance_indicator:
-            hub.display.pixel(0, 0, 100)
-            hub.display.pixel(0, 1, 100)
-            distance_indicator = True
-
-        # Hide junction skipping indicator
-        if junction_indicator and not skipping_junction:
-            hub.display.pixel(0, 3, 0)
-            hub.display.pixel(0, 4, 0)
-            junction_indicator = False
-
         # Return if all conditions met, otherwise update junction state
         if junction_reached and distance_reached and not skipping_junction:
             junctions_crossed += 1
@@ -138,27 +120,36 @@ def linetrack(min_distance: int | float, speed: int | float, *, direction: str =
                 return  # End line tracking
             else:
                 junction_distance = db.distance()
-                # Show junction skipping indicator
-                hub.display.pixel(0, 3, 100)
-                hub.display.pixel(0, 4, 100)
 
         linear_error = sum(reflection * position for reflection, position in zip(line_amounts, SENSOR_POSITIONS))
         sined_error = 3 * sin(linear_error * pi / 6)
         turn_rate = pid_controller.update(sined_error)
         drive_speed = speed
+
         if abs(sined_error) > 1:
             drive_speed = speed - speed * ((abs(sined_error) - 1) / 3)
+            if not slow:
+                slow = True
+                # print("slowing")
+                hub.display.pixel(0, 0, 100)
+        if slow and abs(sined_error) <= 1:
+            slow = False
+            # print("unslowing")
+            hub.display.pixel(0, 0, 0)
 
         # Turn back to the line if the robot ran off the line
-        if sum(line_amounts) < 0.5 and False:  # FIXME: ...
-            if not ran_off:
-                ran_off = True
-                off_distance = db.distance()
-            turn_rate = 100
-            drive_speed = 100
-        elif ran_off:
-            ran_off = False
-            db.reset(off_distance, 0)
+        if sum(line_amounts) < 0.5:
+            turn_rate = 100 if intercept_direction == "right" else -100
+            drive_speed = 50
+            intercept = True
+            # print("intercepting")
+            hub.display.pixel(0, 2, 100)
+        elif intercept and sum(line_amounts) >= 0.5:
+            intercept = False
+            # print("intercepted")
+            hub.display.pixel(0, 2, 0)
+
+        print(drive_speed)
 
         db.drive(drive_speed, turn_rate)
 
@@ -219,33 +210,25 @@ def main() -> None:
 
     wait(500)
 
-    while True:
-        linetrack(1600, 500)
+    while False:
+        linetrack(1600, 530)
 
     # Keep doing lap 1
     while True:
-        linetrack(533 - 40, 700)  # PS-T1i
-        hub.speaker.beep(next(notes), 50)
-        linetrack(213 + 40 + 50 - 80, 400)  # HACK: T1i-T1o
-        hub.speaker.beep(next(notes), 50)
-        linetrack(482 - 50 - 23, 700)  # T1o-T2i
-        hub.speaker.beep(next(notes), 50)
-        linetrack(123 + 23, 500)  # T2i-T2o
-        hub.speaker.beep(next(notes), 50)
-        linetrack(323 - 150, 700, direction="both")  # T2o-J1
-        hub.speaker.beep(next(notes), 50)
+        linetrack(533 - 80, 530)  # FIXME: PS-T1i
+        linetrack(213 + 100, 300)  # HACK: T1i-T1o
+        linetrack(482 - 100, 530)  # T1o-T2i
+        linetrack(123 + 23, 300)  # T2i-T2o
+        linetrack(323 - 150, 530, direction="both")  # T2o-J1
+        db.brake()
+        bye
         turn_right()
-        linetrack(397 - 36 - 40, 700)  # J1-T3i
-        hub.speaker.beep(next(notes), 50)
-        linetrack(0 + 40 + 50, 200)  # T3i-T3o
-        hub.speaker.beep(next(notes), 50)
+        linetrack(397 - 36 - 40 - 200, 700)  # J1-T3i
+        linetrack(0 + 40 + 50 + 200, 150)  # FIXME: T3i-T3o
         linetrack(0, 700, direction="both")  # FIXME: T3o-J2
-        hub.speaker.beep(next(notes), 50)
         turn_right()
         linetrack(538 - 36 - 40, 700)  # J2-T4i
-        hub.speaker.beep(next(notes), 50)
         linetrack(217, 400)  # FIXME: T4i-T4o
-        hub.speaker.beep(next(notes), 50)
         # linetrack(306 - 50, 700)  # T4o-S/F
 
 
